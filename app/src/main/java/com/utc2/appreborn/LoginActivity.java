78525bba4177cfd36bb32846c1b46def8bfdc663
Hinh Vinh Phat <hinhvinhphat@gmail.com>
2026-03-29 15:30:54 +0700

    USA-53 Integrate Gmail Authentication

--- app/src/main/java/com/utc2/appreborn/LoginActivity.java
+++ app/src/main/java/com/utc2/appreborn/LoginActivity.java
@@ -13,8 +13,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
 
     EditText email, password;
-    Button loginBtn;
-
+    Button loginBtn, skipBtn;
     FirebaseAuth auth;
 
     @Override
@@ -25,28 +24,57 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         loginBtn = findViewById(R.id.loginBtn);
+        skipBtn = findViewById(R.id.skipBtn);
 
         auth = FirebaseAuth.getInstance();
 
+        // LOGIN BUTTON
         loginBtn.setOnClickListener(v -> {
 
             String userEmail = email.getText().toString();
             String userPass = password.getText().toString();
-
+            //check xem có để trống email ko
+            if(userEmail.isEmpty()){
+                email.setError("Nhập email đi bro :V");
+                email.requestFocus();
+                return;
+            }
+            //check xem có để trống mk ko
+            if(userPass.isEmpty()){
+                password.setError("Nhập password đi bro :V");
+                password.requestFocus();
+                return;
+            }
             auth.signInWithEmailAndPassword(userEmail, userPass)
                     .addOnCompleteListener(task -> {
 
-                        if(task.isSuccessful()){
-                            Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
 
                             startActivity(new Intent(this, MainActivity.class));
                             finish();
-
-                        }else{
-                            Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
                         }
-
                     });
+        });
+
+        // ✅ SKIP LOGIN
+        skipBtn.setOnClickListener(v -> {
+
+            auth.signInWithEmailAndPassword(
+                    "test123@gmail.com",
+                    "123456"
+            ).addOnCompleteListener(task -> {
+
+                if(task.isSuccessful()){
+                    startActivity(new Intent(this, MainActivity.class));
+                    Toast.makeText(this, "Skip Success", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(this,"Skip Failed",Toast.LENGTH_SHORT).show();
+                }
+            });
 
         });
     }
--- app/src/main/java/com/utc2/appreborn/MainActivity.java
+++ app/src/main/java/com/utc2/appreborn/MainActivity.java
@@ -2,12 +2,14 @@ package com.utc2.appreborn;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Đã vào MainActivity", Toast.LENGTH_LONG).show();
     }
 }
